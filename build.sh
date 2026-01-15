77b66e9fd67e4d6d223b7446733cb87dfe5e62a1
iamblackornot <nkzshinnnik@gmail.com>
2026-01-16 02:50:46 +0300

    updated build script

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 sudo dotnet publish -c Release -r linux-x64 --self-contained true
 mkdir bin/Release/net8.0/linux-x64/logs
+touch bin/Release/net8.0/linux-x64/logs/err.log
+touch bin/Release/net8.0/linux-x64/logs/out.log
+touch bin/Release/net8.0/linux-x64/logs/combined.log
 sudo chmod -R 755 bin
